4bb86b172445a9eb968fe26eeb314488bad5130a
Mat <matiasiparraguirre.info@gmail.com>
2026-01-27 13:39:48 +0100

    correccion barra busqueda

--- barra-busqueda.js
+++ barra-busqueda.js
@@ -1,26 +1,32 @@
-document.querySelector('searchInput').addEventListener('input'),
+document.querySelector('searchInput').addEventListener('input',
+
+// let obtenerSkills => Response.json
 function(e){
     // funcion añade evento de escucha al input, lo pasa a minuscula, busca dentro de los filtros
 
     const searchTerm = e.target.value.toLowerCase();
     const allFilters = [
-        ...skills, ...catergorias,...localidad,...modalidad,...provicia,...nombre,...ciudad
+        // ...s.nombre, ...u.nombre
     ];
     const filtered = allFilters.filter(
-        skill =>
-            skill.id.toLowerCase().includes(searchTerm)
+        skills =>
+            skills.nombre.toLowerCase().includes(searchTerm)
     );
     // añadir las busquedas y mostrarlas
     if(searchTerm){
         // deja en blanco el mainGrid dentro del html. esto deberia cambiar a verse todos.
         document.querySelector('mainGrid').innerHTML='';
-        filtered.forEach(skill => createCard(skill, 'mainGrid'));
+
+        filtered.forEach(skills => createCard(skills,'mainGrid'));
     }
     else {
         document.querySelector('mainGrid').innerHTML='';
-        skills.forEach(skill => createCard(skill,'mainGrid'));
+
+        skills.forEach(skills => createCard(skills,'mainGrid'));
+        
     }
 
-}
+});
 
 //falta revisar el nombre de las id/clases para que coincidan 
+// crear una funcion que cree contenedor de skills dentro de cards
